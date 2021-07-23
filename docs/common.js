d18351caae85bd96f61adb04356ceef258be9d76
DDJ-Seo <dongjoo0714@gmail.com>
2021-07-23 20:32:25 +0900

    실제화면 코딩

---
+++ docs/common.js
@@ -0,0 +1,69 @@
+gsap.from('.img-1', {
+  ease: Expo.easeIn,
+  opacity:0,
+  duration:0.5,
+  scrollTrigger:{
+    trigger:'.img-1',
+    start:'0 90%'
+  }
+});
+
+gsap.from('.img-2', {
+  ease: Expo.easeIn,
+  opacity:0,
+  duration:1.5,
+  scrollTrigger:{
+    trigger:'.img-2',
+    start:'0 90%'
+  }
+});
+
+gsap.from('.img-3', {
+  ease: Expo.easeIn,
+  opacity:0,
+  duration:0.5,
+  scrollTrigger:{
+    trigger:'.img-3',
+    start:'0 90%'
+  }
+});
+
+gsap.from('.img-4', {
+  ease: Expo.easeIn,
+  opacity:0,
+  duration:1.5,
+  scrollTrigger:{
+    trigger:'.img-3',
+    start:'0 90%'
+  }
+});
+
+gsap.from('.img-5', {
+  ease: Expo.easeIn,
+  opacity:0,
+  duration:2.5,
+  scrollTrigger:{
+    trigger:'.img-3',
+    start:'0 90%'
+  }
+});
+
+gsap.from('.img-6', {
+  ease: Expo.easeIn,
+  opacity:0,
+  duration:3,
+  scrollTrigger:{
+    trigger:'.img-3',
+    start:'0 90%'
+  }
+});
+
+gsap.from('.img-7', {
+  ease: Expo.easeIn,
+  opacity:0,
+  duration:3,
+  scrollTrigger:{
+    trigger:'.img-3',
+    start:'0 90%'
+  }
+});
